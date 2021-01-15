3a614a7f535c8a9473dd4a6400c3bd3eab3669d3
Dylan Lom <dylanlom@gmail.com>
2021-01-16 01:45:26 +1100

    Remove echo dependency
    The shmd program now interprets the $(...) command substitution on it's
    own. This means that basic.shmd now compiles correctly!
    
    `execute_command` has been renamed to `command_execute` to match the
    naming of `command_substitute`. It also now strips up to one trailing
    newline from the command's output.

--- shmd.c
+++ shmd.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "util.h"
 
 const char* argv0;
 
-char* execute_command(const char* command) {
+char* command_execute(const char* command) {
     FILE *fp;
     fp = popen(command, "r");
     if (fp == NULL) edie("popen: ");
@@ -30,22 +31,62 @@ char* execute_command(const char* command) {
     free(buf);
     pclose(fp);
 
+    result = str_trimr(result, '\n', 1);
     return result;
 }
 
-char* process_shmd(char* input, ...) {
-    return execute_command(str_concat(3, "echo -n \"", input, "\""));
+char* command_substitute(FILE* fp) {
+    size_t command_size = 100;
+    size_t command_i = 0;
+    char* command = calloc(command_size, sizeof(char));
+    if (command == NULL) edie("calloc: ");
+    memset(command, '\0', command_size);
+
+    int sub_parens = 0; /* track nested parans inside of command */
+    char c;
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == ')' && sub_parens == 0) break;
+        else if (c == ')') sub_parens--;
+        else if (c == '(') sub_parens++;
+
+        if (command_i >= command_size) {
+            command_size += 100;
+            command = realloc(command, command_size);
+            if (command == NULL) edie("realloc: ");
+            memset(command + command_i, '\0', command_size - command_i);
+        }
+
+        command[command_i] = c;
+        command_i++;
+    }
+
+    return command_execute(command);
+}
+
+int process_input(FILE* fp) {
+    char b, c;
+
+    b = fgetc(fp);
+    while (b != EOF) {
+        c = fgetc(fp);
+        if (b == '$' && c == '(') {
+            printf("%s", command_substitute(fp));
+            /*
+             * command_substitute consumes from fp to after the closing ')' of
+             * the substitution, we then need to get the next character from fp
+             * for the next iteration of the loop.
+             */
+            b = fgetc(fp);
+        } else {
+            putchar(b);
+            b = c;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
     SET_ARGV0();
-    size_t buf_size = 1000;
-    char* buf = calloc(buf_size, sizeof(char));
-    if (buf == NULL) edie("calloc: ");
-
-    while (fgets(buf, buf_size, stdin) != NULL) {
-        printf("%s", process_shmd(buf));
-    }
-
-    return EXIT_SUCCESS;
+    return process_input(stdin);
 }
